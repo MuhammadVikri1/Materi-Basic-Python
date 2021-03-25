50b196aa89e13d17244fa52bf8c79669bf0edc1a
MuhammadVikri1 <muhammadvikri000@gmail.com>
2021-03-25 21:07:29 +0700

    Semangat Belajar ya

---
+++ Function Parameter/Argumen.py
@@ -0,0 +1,19 @@
+def my_function(name):
+print("Hello " + name )
+my_function("James")
+
+
+#Default Parameter
+
+def my_function(name=""):
+print("Hello " + name )
+my_function("James")
+
+#Function Keyword Parameter
+
+def my_function(child3, child2, child1):
+print("The youngest child is " + child3)
+my_function(child1 = "Emil", child2 = "Tobias", child3 = "Linus")
+
+
+
